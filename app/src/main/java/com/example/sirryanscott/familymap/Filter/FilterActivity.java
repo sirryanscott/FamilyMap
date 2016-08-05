16265b2c12cad884d26712a7fc6c5daefe96490e
Ryan Struthers <ryan.s.struthers@gmail.com>
2016-08-05 15:18:12 -0600

    added Search, Filter, and Settings activities and fragments

---
+++ app/src/main/java/com/example/sirryanscott/familymap/Filter/FilterActivity.java
@@ -0,0 +1,15 @@
+package com.example.sirryanscott.familymap.Filter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sirryanscott.familymap.R;
+
+public class FilterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+    }
+}
---
+++ app/src/main/java/com/example/sirryanscott/familymap/Search/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.example.sirryanscott.familymap.Search;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sirryanscott.familymap.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
